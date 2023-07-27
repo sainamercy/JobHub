e1d99d0eb115c1e5cdec84d47d4d09283c2e12a3
sainamercy <sainamercy@gmail.com>
2023-07-27 08:17:18 +0300

    created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,25 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HomeLayout, Register, Login, DashboardLayout } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/dashboard',
+    element: <DashboardLayout />,
+  },
+]);
 const App = () => {
-  return <h1>JobHub App</h1>;
+  return <RouterProvider router={router} />;
 };
 export default App;
